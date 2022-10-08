5254aac68b9fab77323b82dffeef3f69284bd841
HNarendranath <81422341+HNarendranath@users.noreply.github.com>
2022-10-08 18:27:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -199,18 +199,7 @@ function getWord() {
 	/*var rndnumber = Math.floor(Math.random() * 113);
 	var word = words[rndnumber];
 	theWord = word;*/
-	const options = {
-		method: 'GET',
-		headers: {
-			'X-RapidAPI-Key': 'c7eff6f615msh1f45f7fd62fb666p1890cejsn4ca8c6c211b4',
-			'X-RapidAPI-Host': 'pictionary-charades-word-generator.p.rapidapi.com'
-			}
-		};
-
-	fetch('https://pictionary-charades-word-generator.p.rapidapi.com/charades?difficulty=medium', options)
-		.then(response => response.json())
-		.then(response => theWord = response)
-		.catch(err => console.error(err));
+	theWord = https://random-word-api.herokuapp.com/word?lang=en
 	document.getElementById("word").innerHTML = theWord;
 	sendWord(word);
 }
